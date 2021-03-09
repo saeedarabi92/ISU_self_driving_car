f5a7ce463b519ef624eccb39c8349dff2ccfb062
saeedarabi92 <arabi@iastate.edu>
2021-03-08 20:54:35 -0600

    update file

--- src/gem_control/script/odom_to_baselink_tf.py
+++ src/gem_control/script/odom_to_baselink_tf.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    odom_to_baselink_tf.py                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: saeed <arabi@iastate.edu>                  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/03/08 18:17:24 by saeed             #+#    #+#              #
+#    Updated: 2021/03/08 18:17:24 by saeed            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 import rospy
 import math
 import sys
@@ -14,42 +27,34 @@ from geometry_msgs.msg import TransformStamped
 from nav_msgs.msg import Odometry
 
 global tf_pub
-tf_pub        = tf2_ros.TransformBroadcaster()
+tf_pub = tf2_ros.TransformBroadcaster()
+
 
 def odom_callback(data):
 
-    odom                      = Odometry()
-    odom.header.frame_id      = '/odom'
-    odom.child_frame_id       = 'base_footprint'
-    odom.header.stamp         = rospy.Time.now()
-    odom.pose                 = data.pose
+    odom = Odometry()
+    odom.header.frame_id = '/odom'
+    odom.child_frame_id = 'base_footprint'
+    odom.header.stamp = rospy.Time.now()
+    odom.pose = data.pose
     odom.pose.pose.position.x = odom.pose.pose.position.x
     odom.pose.pose.position.y = odom.pose.pose.position.y
     odom.twist = data.twist
+    tf = TransformStamped(header=Header(
+                          frame_id=odom.header.frame_id,
+                          stamp=odom.header.stamp),
+                          child_frame_id=odom.child_frame_id,
+                          transform=Transform(
+                          translation=odom.pose.pose.position,
+                          rotation=odom.pose.pose.orientation))
 
-    tf = TransformStamped(header         = Header(
-                          frame_id       = odom.header.frame_id,
-                          stamp          = odom.header.stamp),
-                          child_frame_id = odom.child_frame_id,
-                          transform      = Transform(
-                          translation    = odom.pose.pose.position,
-                          rotation       = odom.pose.pose.orientation))
-
-    
     tf_pub.sendTransform(tf)
 
 
-
 if __name__ == '__main__':
-
     try:
-
-        rospy.init_node('odom_to_baselink_tf', anonymous = True)
-
+        rospy.init_node('odom_to_baselink_tf', anonymous=True)
         rospy.Subscriber('/odom', Odometry, odom_callback)
-
         rospy.spin()
-
     except rospy.ROSInterruptException:
-
         pass
