9b95c48b19fd6413a1a827131024d58c7cfa9b11
saeedarabi92 <arabi@iastate.edu>
2021-03-10 20:37:06 -0600

    add visualizer.py

---
+++ src/gem_control/script/helper/visualizer.py
@@ -0,0 +1,45 @@
+from visualization_msgs.msg import MarkerArray, Marker
+import rospy
+import tf
+
+
+def pose_to_marker(x, y, theta, frame):
+    marker = Marker()
+    marker.header.frame_id = frame  # "/odom"
+    marker.header.stamp = rospy.Time.now()
+    marker.pose.position.x = x
+    marker.pose.position.y = y
+    marker.pose.position.z = 0
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, theta)
+    marker.pose.orientation.x = quaternion[0]
+    marker.pose.orientation.y = quaternion[1]
+    marker.pose.orientation.z = quaternion[2]
+    marker.pose.orientation.w = quaternion[3]
+    marker.scale.x = 0.3
+    marker.scale.y = 0.05
+    marker.scale.z = 0.1
+    marker.color.r = 0.0
+    marker.color.g = 1.0
+    marker.color.b = 0.0
+    marker.color.a = 1
+    marker.type = Marker.ARROW
+    marker.action = marker.ADD
+    return marker
+
+
+def path_to_marker(path):
+    Path = MarkerArray()
+    pose_id = 0
+    idx = 0
+    while True:
+        x = path[idx][0]
+        y = path[idx][1]
+        yaw = path[idx][2]
+        pose_marker = pose_to_marker(x, y, yaw, "/odom")
+        pose_marker.id = pose_id
+        Path.markers.append(pose_marker)
+        pose_id += 1
+        idx += 1
+        if idx == len(path):
+            break
+    return Path
